9cc6cdef2b055c5dda819a58ba999654655a5ff9
Manuel Zavatta <manuel.zavatta@gmail.com>
2024-11-26 13:41:23 +0100

    fix

--- src/Engine.php
+++ src/Engine.php
@@ -12,25 +12,25 @@ class Engine {
 
   public static function run():void {
     $today = date('Y-m-d');
-    //$today = '2024-11-27';
-    $HTML = new Html();
+    $today = '2024-11-27';
+    $html = new Html();
     if(date('l',strtotime($today)) !== "Wednesday"){
-      $HTML->addSection(new Nothing());
+      $html->addSection(new Nothing());
     } else {
       $dataset = new Dataset($today);
       if($_SERVER['REQUEST_URI'] === '/submit') {
         $dataset->addSubscriber($_POST['name'],$_POST['choose']);
         header("location: /");
       }
-      $HTML->addSection(new Event($dataset));
-      $HTML->addSection(new Choices($dataset));
+      $html->addSection(new Event($dataset));
+      $html->addSection(new Choices($dataset));
       if(date('H') < 18) {
-        $HTML->addSection(new Form());
+        $html->addSection(new Form());
       } else {
-        $HTML->addSection(new Closed());
+        $html->addSection(new Closed());
       }
     }
-    $HTML->render();
+    $html->render();
   }
 
 }
